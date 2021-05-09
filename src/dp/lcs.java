b4a0fd189fc850e93bb89c1ff7289bc928073865
pengxinjie <1370639979@qq.com>
2021-05-09 13:58:03 +0800

    lcs递归实现

--- src/dp/lcs.java
+++ src/dp/lcs.java
@@ -1,6 +1,5 @@
 package dp;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -13,12 +12,13 @@ public class lcs {
         while (scanner.hasNextLine()) {
             String str1 = scanner.nextLine().toLowerCase();
             String str2 = scanner.nextLine().toLowerCase();
-            System.out.println(findLCS(str1, str1.length(), str2, str2.length()));
+//            System.out.println(findLcs(str1, str1.length(), str2, str2.length()));
+            System.out.println(findLcs2(str1,str2));
         }
     }
 
-    //算法实现
-    public static int findLCS(String A,int n,String B,int m){
+    //算法实现(迭代)
+    public static int findLcs(String A, int n, String B, int m){
         //构建表
         int[][] dp = new int[n+1][m+1];
         //初始化表
@@ -39,5 +39,17 @@ public class lcs {
             }
         }
         return dp[n][m];
+    }
+
+    //算法实现(递归)
+    public static int findLcs2(String A,String B){
+        if (A.length() == 0 || B.length() == 0){
+            return 0;
+        }
+        if (A.charAt(0) == B.charAt(0)){
+            return 1+findLcs2(A.substring(1),B.substring(1));
+        }else{
+            return Math.max(findLcs2(A,B.substring(1)),findLcs2(A.substring(1),B));
+        }
     }
 }
